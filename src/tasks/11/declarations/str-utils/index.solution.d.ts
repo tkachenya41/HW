7e0c9bd45c21b172a5719020d1301e2acb749677
Stanislav <ess.webprog@gmail.com>
2023-04-14 11:48:13 +0300

    fix: remove solutions

--- src/tasks/11/declarations/str-utils/index.solution.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'str-utils' {
-  type StringUtil = (input: string) => string;
-
-  export const strReverse: StringUtil;
-  export const strToLower: StringUtil;
-  export const strToUpper: StringUtil;
-  export const strRandomize: StringUtil;
-  export const strInvertCase: StringUtil;
-}
